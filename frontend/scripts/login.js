0f6be8dbb66a889da46f0af54252ff721576649c
Idana Noely Wagner Cabrera <idanawagner@gmail.com>
2023-11-18 17:42:27 -0300

    Funcionalidad login agregada

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,7 +1,6 @@
 function mostrarFormulario(tipo) {
     const loginContainer = document.querySelector('.login-container');
     const registerContainer = document.querySelector('.register-container');
-
     if (tipo === 'login') {
         loginContainer.style.display = 'block';
         registerContainer.style.display = 'none';
@@ -14,32 +13,51 @@ function mostrarFormulario(tipo) {
 function mostrarPago(){
     const pago_container = document.querySelector('.pago_container');
     pago_container.style.display = 'flex';
-    console.log('hola');
 }
 
 function Registro() {
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
 
-    const usuario = {
-        nombre,
-        apellido,
-        email,
-        password
-    };
 
-    fetch('http://localhost:3000/usuarios', {
-        method: 'POST',
-        body: JSON.stringify(usuario),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            mostrarFormulario('login');
-        });
 }
+const URL=`http://127.0.0.1:5200`
+class Usuario{
+    constructor(username, password){
+        this.username = username;
+        this.password = password;
+    }
+    
+    login(){  
+        const requestOptions = {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(this.username + ":" + this.password)
+            },
+        };
+
+        fetch(URL + '/login', requestOptions)
+          .then(response => response.json())
+          .then(data => {
+                if(data.token){
+                    localStorage.setItem('token', data.token)
+                    window.location.href = 'home.html'
+                }else{
+                    document.getElementById('errorLogin').innerHTML = 'Usuario o contraseña incorrectos'
+                }
+          })
+          .catch(error => {
+              console.error('Error al realizar la solicitud:', error);
+          });
+          
+        }
+        
+    }
+
+let btnLogin = document.getElementById('btn-login')
+    
+btnLogin.addEventListener('click', () =>{
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password-login').value;
+    let usuario1 = new Usuario(username, password)
+    usuario1.login();
+})
